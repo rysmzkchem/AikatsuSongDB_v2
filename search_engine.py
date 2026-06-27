fc2e5bcb6feaff98d62390f4aea9a096b58bdac9
rysmzkchem <GitHub>
2026-06-27 22:21:34 +0900

    fix: update config and search engine

--- search_engine.py
+++ search_engine.py
@@ -32,7 +32,7 @@ def has_missing_required(data):
 
 def search_aikatsu_wiki(title: str):
     try:
-        search_url = "https://aikatsu.fandom.com/ja/wiki/Special:Search"
+        search_url = "https://all-aikatsu.fandom.com/wiki/All_Aikatsu!_Wiki"
 
         params = {
             "query": title
